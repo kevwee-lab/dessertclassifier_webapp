26d580e5cddf7e3b90e1f6517b66e1b70b105b11
Kevin <61779569+kevwee-lab@users.noreply.github.com>
2021-04-28 14:38:01 +0800

    Update foodclass_app.py

--- foodclass_app.py
+++ foodclass_app.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from tensorflow import keras
 from keras import models
 import streamlit as st
-import cv2
+from cv2 import cv2
 import numpy as np
 from img_class import import_and_predict
 
