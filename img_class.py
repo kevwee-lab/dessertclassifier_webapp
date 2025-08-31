73d7a7932fb7d22ffb6da57bfa3a97622b8571cd
Kevin <61779569+kevwee-lab@users.noreply.github.com>
2025-08-31 13:53:44 +0800

    Update img_class.py

--- img_class.py
+++ img_class.py
@@ -9,7 +9,7 @@ model = tf.keras.models.load_model('./finalmodel/final_model.h5')
 def import_and_predict(image_data):
     # Resize and preprocess image
     size = (224, 224)
-    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    image = ImageOps.fit(image_data, size, Image.Resampling.LANCZOS)
     img_array = np.array(image) / 255.0  # scale pixels
     img_array = np.expand_dims(img_array, axis=0)  # add batch dimension
     
